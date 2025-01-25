02e56ad682f07b3da2f858406cf0e7d22617c70c
Natalia <nataxawolf@gmail.com>
2025-01-25 15:55:43 +0300

    Cteated table

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,40 @@
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <div>
+        <div>
+          <hi>Список контактов</hi>
+        </div>
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th>№</th>
+                <th>Имя контакта</th>
+                <th>Телефон</th>
+                <th>E-mail</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th>1</th>
+                <th>fff</th>
+                <th>fff</th>
+                <th>fff</th>
+              </tr>
+
+              <tr>
+                <th>2</th>
+                <th>ffff</th>
+                <th>sss</th>
+                <th>ssss</th>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
+
   );
 }
 
