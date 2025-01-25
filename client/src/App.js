a121f05f94ff65ad85e580a7663145c4253af98a
Natalia <nataxawolf@gmail.com>
2025-01-25 21:57:16 +0300

    Настройка Get запроса

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,19 @@
 import FormContact from "./layout/FormContact/FormContact";
 import TableContact from "./layout/TableContact/TableContact";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const baseApiUrl = process.env.REACT_APP_API_URL;
 const App = () => {
-  const url = `${baseApiUrl}/contacts`; 
-  axios.get(url).then(
-    res=>console.log(res.data)
-  );
-  const [contacts, setContacts] = useState(
-    [
-      { id: 1, name: 'John Doe', phone: '+1 (123) 456-7890', email: 'john@example.com' },
-      { id: 2, name: 'Jane Doe', phone: '+2 (987) 654-3210', email: 'jane@example.com' },
-      { id: 3, name: 'Michael Johnson', phone: '+3 (555) 123-4567', email: 'michael@example.com' },
-      { id: 4, name: 'Sarah Smith', phone: '+4 (999) 888-7777', email: 'sarah@example.com' },
-    ]);
+  const [contacts, setContacts] = useState([]);
 
+  const url = `${baseApiUrl}/contacts`;
+  useEffect(() => {
+    axios.get(url).then(
+      res => setContacts(res.data)
+    );
+  }, []);
+  
   const addContact = (contactName, contactPhone, contactEmail) => {
     const newId = contacts.length > 0 ? Math.max(...contacts.map(e => e.id)) + 1 : 1;
     const item = {
--- client/src/layout/TableContact/TableContact.jsx
+++ client/src/layout/TableContact/TableContact.jsx
@@ -1,7 +1,5 @@
 import RowTableContact from "./components/RowTableContact";
 
-
-
 const TableContact = (props) => {
     return (
 
