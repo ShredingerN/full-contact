9eaa10caeafd482e29170f04422c24c1d6fe9c29
Natalia <nataxawolf@gmail.com>
2025-01-25 18:56:43 +0300

    форма добавления контакта

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,31 @@
+import FormContact from "./layout/FormContact/FormContact";
 import TableContact from "./layout/TableContact/TableContact";
 import { useState } from 'react';
 
 const App = () => {
   const [contacts, setContacts] = useState(
     [
-      // { id: 1, name: 'John Doe', phone: '+1 (123) 456-7890', email: 'john@example.com' },
-      // { id: 2, name: 'Jane Doe', phone: '+2 (987) 654-3210', email: 'jane@example.com' },
-      // { id: 3, name: 'Michael Johnson', phone: '+3 (555) 123-4567', email: 'michael@example.com' },
-      // { id: 4, name: 'Sarah Smith', phone: '+4 (999) 888-7777', email: 'sarah@example.com' },
+      { id: 1, name: 'John Doe', phone: '+1 (123) 456-7890', email: 'john@example.com' },
+      { id: 2, name: 'Jane Doe', phone: '+2 (987) 654-3210', email: 'jane@example.com' },
+      { id: 3, name: 'Michael Johnson', phone: '+3 (555) 123-4567', email: 'michael@example.com' },
+      { id: 55, name: 'Sarah Smith', phone: '+4 (999) 888-7777', email: 'sarah@example.com' },
     ]);
 
-    const addContact = () => {
-      const item = {
-        id: Math.floor(Math.random() * 100),
-        name: 'New Contact',
-        phone: '+5 (000) 000-0000',
-        email: 'newcontact@example.com',
-      };
-    
-      setContacts([...contacts, item]);
-      console.log('New contact added:', item);
+  const addContact = () => {
+    //компаратор
+    const newId = contacts
+      .sort((a, b) => a.id - b.id)[contacts.length - 1]
+      .id + 1;
+    const item = {
+      id: newId,
+      name: 'New Contact',
+      phone: '+5 (000) 000-0000',
+      email: 'newcontact@example.com',
     };
+
+    setContacts([...contacts, item]);
+    console.log('New contact added:', item);
+  };
 
   return (
     <div className="container mt-5" >
@@ -29,13 +34,7 @@ const App = () => {
           <h1>Список контактов</h1>
         </div>
         <TableContact contacts={contacts} />
-        <div>
-          <button
-            className="btn btn-primary"
-            onClick={() => { addContact() }}>
-            Добавить контакт
-          </button>
-        </div>
+        <FormContact addContact={addContact}/>
         <div className="card-body">
         </div>
       </div>
