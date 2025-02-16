220677731bfeceb7346075343b53a418c12a2726
Natalia <nataxawolf@gmail.com>
2025-02-16 20:23:16 +0300

    доработка клиента: просмотр данных контакта

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,12 @@ import FormContact from "./layout/FormContact/FormContact";
 import TableContact from "./layout/TableContact/TableContact";
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Route } from "react-router-dom";
 
 const baseApiUrl = process.env.REACT_APP_API_URL;
 const App = () => {
   const [contacts, setContacts] = useState([]);
+
 
   const url = `${baseApiUrl}/contacts`;
   useEffect(() => {
@@ -13,18 +15,19 @@ const App = () => {
       res => setContacts(res.data)
     );
   }, []);
-  
+
   const addContact = (contactName, contactPhone, contactEmail) => {
-    const newId = contacts.length > 0 ? Math.max(...contacts.map(e => e.id)) + 1 : 1;
+
     const item = {
-      id: newId,
       name: contactName,
       phone: contactPhone,
       email: contactEmail,
     };
     const url = `${baseApiUrl}/contacts`;
-    axios.post(url, item);
-    setContacts([...contacts, item]);
+    axios.post(url, item).then(
+      response => setContacts([...contacts, response.data])
+    );
+    ;
   };
 
   const deleteContact = (id) => {
@@ -36,18 +39,22 @@ const App = () => {
 
   return (
     <div className="container mt-5" >
-      <div className="card">
-        <div className="card-header">
-          <h1>Список контактов</h1>
-        </div>
-
-        <div className="card-body">
-          <TableContact
-            contacts={contacts}
-            deleteContact={deleteContact} />
-          <FormContact addContact={addContact} />
-        </div>
-      </div>
+      <Routes>
+        <Route path="/" element={
+          <div className="card">
+            <div className="card-header">
+              <h1>Список контактов</h1>
+            </div>
+            <div className="card-body">
+              <TableContact
+                contacts={contacts}
+                deleteContact={deleteContact} />
+              <FormContact addContact={addContact} />
+            </div>
+          </div>
+        }/>
+        <Route path="contact/:id" element={<h1>Page not found</h1>}/>
+      </Routes>
     </div>
 
   );
