81ac356ba96b640ec17759093e757fcdcbddab20
Natalia <nataxawolf@gmail.com>
2025-02-19 00:12:54 +0300

    добавлены конфигурации для middleware

--- api/Program.cs
+++ api/Program.cs
@@ -12,8 +12,14 @@ app.UseSwaggerUI(c =>
 {
     c.SwaggerEndpoint("/swagger/v1/swagger.json", "MY API V1");
 });
+//Порядок обязателен!
+//подключаем конвеер
+app.UseConfigMiddleware();
+//добавляем файлы
 app.UseDefaultFiles();
+//обработка статических файлов клиента
 app.UseStaticFiles();
+//маршрутизация запросов к контроллерам
 app.MapFallbackToController("Index","Fallback");
 app.UseCors("AllowAll");
 app.MapControllers();
