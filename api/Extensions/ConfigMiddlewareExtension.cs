ca62a7c9df6eebbf0ac9a17078145be14a29f12d
Natalia <nataxawolf@gmail.com>
2025-02-19 00:16:04 +0300

    конфигурации для middleware

---
+++ api/Extensions/ConfigMiddlewareExtension.cs
@@ -0,0 +1,9 @@
+
+public static class ConfigMiddlewareExtension
+{
+    public static IApplicationBuilder UseConfigMiddleware(
+        this IApplicationBuilder app)
+    {
+        return app.UseMiddleware<ConfigMiddleware>();
+    }
+}
---
+++ api/Middliware/ConfigMiddleware.cs
@@ -0,0 +1,29 @@
+
+public class ConfigMiddleware
+{
+
+    private readonly RequestDelegate next;
+    public ConfigMiddleware(RequestDelegate next)
+    {
+        this.next = next;
+    }
+    public async Task InvokeAsync(HttpContext context)
+    {
+        if (context.Request.Path == "config.js")
+        {
+            var scheme = context.Request.Scheme;
+            var host = context.Request.Host.Value;
+            var pathBase = context.Request.Path.Value;
+
+            var apiUrl = $"{scheme}://{host}:{pathBase}/api/ContactManager";
+            var config = $@"window.config={{apiUrl: '{apiUrl}'}}";
+            context.Response.ContentType = "application/javascript";
+            await context.Response.WriteAsync(config);
+
+        }
+        else
+        {
+            await this.next(context);
+        }
+    }
+}
